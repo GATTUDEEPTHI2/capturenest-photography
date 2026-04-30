a46bf63b1a66f6b1c1a526fed93c5436d3ccc343
GATTUDEEPTHI2 <gattudeepthi10@gmail.com>
2026-04-30 23:47:27 +0530

    final footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,15 @@ function Footer() {
   return (
     <footer className="footer">
       <p>
-        This website is designed by <span>PixelCraftStudio</span> (Demo Project)
+        This website is designed by{" "}
+        <a
+          href="https://instagram.com/pixel_craftstudio_01"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          PixelCraftStudio
+        </a>{" "}
+        (Demo Project)
       </p>
     </footer>
   );
